dbda3f165f28ff68d858a3b1d2f9999034582a72
Aluno <Aluno@DESKTOP-DRI1TQ1>
2024-04-27 09:44:24 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+int QTE = 0;
+
+//Solicitar o tamanho da lista
+Console.WriteLine("Quantos itens sua lista possuirá? ");
+QTE = int.Parse(Console.ReadLine());
+Console.Clear();
+
+
+string[] lista = new string[QTE];
+
+// solicitar os itens da lista
+for (int i = 0; i < QTE; i++)
+{
+    Console.WriteLine($"{i + 1}° item da lista ");
+    lista[i] = Console.ReadLine();
+    Console.Clear();
+
+
+    // verifica se os itens se repetem na lista
+   
+    string desejaInserir = "";
+    do
+    {
+        Console.WriteLine();
+
+        
+        int posicao = 0;
+        bool encontrou = false;
+
+        for (int i = 0; i < lista.Length; i++)
+        {
+            if (lista[i].ToUpper() == 
+            {
+                posicao = i;
+                encontrou = true;
+            }
+        }
+
+        if (encontrou)
+            Console.WriteLine();
+        else
+            Console.WriteLine();
+
+        Console.WriteLine("Deseja inserir novamente? (s/n)");
+        desejaInserir = Console.ReadLine();
+    } while (desejaInserir != "n");
+}
+
+
+//imprime a lista 
+for (int i = 0; i < QTE; i++)
+{
+    Console.WriteLine($"{i + 1}°{lista[i]} ");
+}
